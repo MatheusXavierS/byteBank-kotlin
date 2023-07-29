6b409fca9098bd920b28cf74e8928b7c68db6e3c
MatheusXavierS <xave.mxs@gmail.com>
2023-07-29 17:02:11 -0300

    Estudando sobre exception

---
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -0,0 +1,12 @@
+package br.com.alura.bytebank
+
+import br.com.alura.bytebank.modelos.Cliente
+import novomodelo.Cliente as NovoCliente
+import br.com.alura.bytebank.testes.testaAutenticacao
+
+fun main() {
+    testaAutenticacao()
+
+   // val cliente = Cliente( "matheus", "0000000", 01234)
+    //val clienteNovo = NovoCliente()
+}
